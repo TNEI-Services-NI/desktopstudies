bd9c0ad23aecb476421dea529858e6a25f171edb
owen-patrick-tnei <owen.patrick@tneigroup.com>
2021-05-10 13:43:50 +0100

    chapelcross gretna 1 working

--- package/flaskapp/assets/3_15_chapelcross-gretna_132kv_1.js
+++ package/flaskapp/assets/3_15_chapelcross-gretna_132kv_1.js
@@ -10,15 +10,15 @@ networks_undrawn["chapelcrossgretna1"]={
     "CHAP1-_M1-_1#1": StraightLine([290,650],"left",40,"132kV"),
     "1105 B_1": StraightLine([290,650],"up",135,"132kV"),
     "CHAP1-_M1-_1#2": StraightLine([290,650],"up",135,"132kV"),
-    "1105 1103_1": StraightLine([290,460],"down",55,"132kV"),
-    "1103 303 A_1": StraightLine([290,460],"up",200,"132kV"),
-    "1103 303 B_1": StraightLine([290,260],"right",430,"132kV"),
-    "1103 303 C_1": StraightLine([720,260],"down",195,"132kV"),
-    "303 305_1": StraightLine([720,510],"up",55,"132kV"),
-    "305 304_1": StraightLine([720,510],"down",65,"132kV"),
-    "305 304_1": StraightLine([720,510],"down",65,"132kV"),
+    "CHAP1-_GRNA1#0": StraightLine([290,460],"down",55,"132kV"),
+    "CHAP1-_GRNA1#2": StraightLine([290,460],"up",200,"132kV"),
+    "CHAP1-_GRNA1#1": StraightLine([290,260],"right",430,"132kV"),
+    "CHAP1-_GRNA1#3": StraightLine([720,260],"down",195,"132kV"),
+    "CHAP1-_GRNA1#4": StraightLine([720,510],"up",55,"132kV"),
+    "GRNA1-#0": StraightLine([720,510],"down",65,"132kV"),
+    "GRNA1-#1": StraightLine([720,510],"down",65,"132kV"),
 
-    "GRNA1 A_1": StraightLine([720,575],"down",75,"132kV"),
+    "GRNA1-#4": StraightLine([720,575],"down",75,"132kV"),
     "GRNA1 B_1": StraightLine([785,650],"left",130,"132kV"),
 
     },
@@ -32,18 +32,18 @@ networks_undrawn["chapelcrossgretna1"]={
 
     breakers:{
         "1105": new Breaker("CHAP1-_M1-_1#2",1),
-        "305": new Breaker("303 305_1",0),
+        "305": new Breaker("CHAP1-_GRNA1#4",0),
 
     },
 
     labels:{
-        1: new Text("GRNA1 B_1", ["GRNA1"], [0,30]),
+        1: new Text("GRNA1-", ["GRNA1"], [0,30]),
         2: new Text("CHAP1-_M1-_1#0", ["CHAP1"], [80,0]),
 
         3: new Text("CHAP1-_M1-_1", ["M1"], [-80,0]),
         4: new Text("CHAP1-_R1-_1", ["R1"], [-80,0]),
 
-        5: new Text("1103 303 B_1", ["CHAPELCROSS - GRETNA 1"], [0,-170], 25),
+        5: new Text("CHAP1-_GRNA1#1", ["CHAPELCROSS - GRETNA 1"], [0,-170], 25),
 
     },
 
@@ -53,9 +53,9 @@ networks_undrawn["chapelcrossgretna1"]={
     isolators:{
         "1106": new Isolator("CHAP1-_M1-_1#0",0,"open"),
         "1104": new Isolator("CHAP1-_M1-_1#0",0.65,"closed"),
-        "1103": new Isolator("1105 1103_1",0,"closed"),
-        "303": new Isolator("1103 303 C_1",1,"closed"),
-        "304": new Isolator("305 304_1",1,"closed"),
+        "1103": new Isolator("CHAP1-_GRNA1#0",0,"closed"),
+        "303": new Isolator("CHAP1-_GRNA1#3",1,"closed"),
+        "304": new Isolator("GRNA1-#0",1,"closed"),
 
     },
 
