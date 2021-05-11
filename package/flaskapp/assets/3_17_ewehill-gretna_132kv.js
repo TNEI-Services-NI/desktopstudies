0eb026dddb280c6375ec5741553e022fbd9fc42e
owen-patrick-tnei <owen.patrick@tneigroup.com>
2021-05-11 09:42:54 +0100

    EWEHILLL windfarm 2, chepcross-grenta 132kV done

--- package/flaskapp/assets/3_17_ewehill-gretna_132kv.js
+++ package/flaskapp/assets/3_17_ewehill-gretna_132kv.js
@@ -2,11 +2,13 @@
 //scale of 1000 x 1000, readjust with math...
 networks_undrawn["ewehillgretna"]={
     lines:{
-    "GRNA1": StraightLine([630,105],"right",130,"132kV"),
-    "804 805 803": StraightLine([700,340],"up",235,"132kV"),
-    "803 113": StraightLine([700,340],"left",430,"132kV"),
-    "113": StraightLine([270,340],"down",170,"132kV"),
-    "204 132kV": StraightLine([270,380],"left",55,"132kV"),
+    "GRNA1-": StraightLine([630,105],"right",130,"132kV"),
+    "GRNA1-#0": StraightLine([700,205],"up",100,"132kV"),
+    "GRNA1-_EWEH": StraightLine([700,340],"up",135,"132kV"),
+
+    "GRNA1-_EWEH#1": StraightLine([700,340],"left",430,"132kV"),
+    "GRNA1-_EWEH#2": StraightLine([270,340],"down",170,"132kV"),
+    "GRNA1-_EWEH#3": StraightLine([270,380],"left",55,"132kV"),
     "204 0V": StraightLine([220,380],"left",55,"0V"),
 
     "GRID 1A": StraightLine([270,505],"down",315,"33kV"),
@@ -25,7 +27,7 @@ networks_undrawn["ewehillgretna"]={
     },
 
     busbars:{
-     "GRNA1": StraightLine([630,105],"right",130,"132kV"),
+     "GRNA1-": StraightLine([630,105],"right",130,"132kV"),
     "BOARD B": StraightLine([615,705],"right",145,"33kV"),
 
 
@@ -33,8 +35,7 @@ networks_undrawn["ewehillgretna"]={
     },
 
     breakers:{
-        "805": new Breaker("804 805 803",0.6),
-        //todo something is up with Grid 1A and WG2
+        "805": new Breaker("GRNA1-#0",0),
         "GRID 1A": new Breaker("GRID 1A",0.33),
         "WG2": new Breaker("GRID 1A",0.75),
         "GRID 1B": new Breaker("GRID 1B",0.5),
@@ -45,11 +46,11 @@ networks_undrawn["ewehillgretna"]={
     labels:{
         1: new Text("2L5", ["CROSSDYKES 1"], [0,70]),
         2: new Text("3L5", ["CROSSDYKES 2"], [0,70]),
-        3: new Text("113", ["EWEHII","132kV"], [60,-10]),
+        3: new Text("GRNA1-_EWEH#2", ["EWEHILL","132kV"], [60,-10]),
         4: new Text("GRID 1A", ["EWEHII","33kV"], [60,40]),
 
-        5: new Text("GRNA1",["GRNA1"], [100,30]),
-        6: new Text("803 113",["EWE HILL - GRETNA"], [0,-300], 25),
+        5: new Text("GRNA1-",["GRNA1"], [100,30]),
+        6: new Text("GRNA1-_EWEH#1",["EWE HILL - GRETNA"], [0,-300], 25),
 
         7: new Text("204 0V",["FUTURE","CONNECTION"], [-70,0]),
 
@@ -61,16 +62,16 @@ networks_undrawn["ewehillgretna"]={
         },
 
     tx:{
-        "GT1 90MVA" : new Tx("113",1,"","","132kV"),
+        "GT1 90MVA" : new Tx("GRNA1-_EWEH#2",1,"","","132kV"),
         "GRID 1A LV" : new Tx("GRID 1A LV",1,"","","0V"),
 
     },
 
     isolators:{
-        "804": new Isolator("804 805 803",0.8),
-        "803": new Isolator("804 805 803",0.4),
-        "113": new Isolator("113",0.6),
-        "204": new Isolator("204 132kV",1,"open"),
+        "804": new Isolator("GRNA1-#0",0.5),
+        "803": new Isolator("GRNA1-_EWEH",0.6),
+        "113": new Isolator("GRNA1-_EWEH#2",0.6),
+        "204": new Isolator("GRNA1-_EWEH#3",1,"open"),
 
         "1L4A": new Breaker("GRID 1A",0.5),
 
